c4bab6f398f10b477c4730cb0ac3572a86d553f7
Oleksandr Chumak <ochumak@eagleprojects.it>
2025-04-09 19:26:43 +0200

    Creati classi Studente e StudenteTester

---
+++ giorno3/src/it/prova/model/Studente.java
@@ -0,0 +1,17 @@
+package it.prova.model;
+
+public class Studente {
+    private String nome;
+    private String cognome;
+    private int eta;
+
+    public Studente() {
+
+    }
+
+    public Studente(String nome, String cognome, int eta) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.eta = eta;
+    }
+}
---
+++ giorno3/src/it/prova/test/StudenteTester.java
@@ -0,0 +1,11 @@
+package it.prova.test;
+
+import it.prova.model.Studente;
+
+public class StudenteTester {
+
+    public static void main (String[] args) {
+        Studente M001 = new Studente("Filippo", "Borghesi", 18);
+        Studente M002 = new Studente("Germana", "Caldini", 20);
+    }
+}
