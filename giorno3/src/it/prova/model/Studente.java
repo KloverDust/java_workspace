e3d7ee61d8b3e9d9885afeb737418d7db2789616
Oleksandr Chumak <ochumak@eagleprojects.it>
2025-04-09 19:38:28 +0200

    Aggiunto metodo sonoTuttiPiuGiovaniDiMe

--- giorno3/src/it/prova/model/Studente.java
+++ giorno3/src/it/prova/model/Studente.java
@@ -4,14 +4,57 @@ public class Studente {
     private String nome;
     private String cognome;
     private int eta;
+    float mediaScolastica;
 
     public Studente() {
 
     }
 
-    public Studente(String nome, String cognome, int eta) {
+    public Studente(String nome, String cognome, int eta, float mediaScolastica) {
         this.nome = nome;
         this.cognome = cognome;
         this.eta = eta;
+        this.mediaScolastica = mediaScolastica;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public int getEta() {
+        return eta;
+    }
+
+    public void setEta(int eta) {
+        this.eta = eta;
+    }
+
+    public float getMediaScolastica() {
+        return mediaScolastica;
+    }
+
+    public void setMediaScolastica(float mediaScolastica) {
+        this.mediaScolastica = mediaScolastica;
+    }
+
+    public boolean sonoTuttiPiuGiovaniDiMe(Studente[] corso){
+        int miaEta= this.eta;
+        for (Studente matricola: corso) {
+            if(miaEta<=matricola.getEta())
+                return false;
+        }
+        return true;
     }
 }
--- giorno3/src/it/prova/test/StudenteTester.java
+++ giorno3/src/it/prova/test/StudenteTester.java
@@ -5,7 +5,20 @@ import it.prova.model.Studente;
 public class StudenteTester {
 
     public static void main (String[] args) {
-        Studente M001 = new Studente("Filippo", "Borghesi", 18);
-        Studente M002 = new Studente("Germana", "Caldini", 20);
+        Studente m001 = new Studente("Filippo", "Borghesi", 35, 25);
+        Studente m002 = new Studente("Germana", "Caldini", 20, 21);
+        Studente m003 = new Studente("Mario", "Rossi", 22, 18);
+        Studente m004 = new Studente("Luca", "Verdi", 20, 19);
+        Studente m005 = new Studente("Anna", "Bianchi", 19, 30);
+
+        Studente[] corsoMate = new Studente[] {m002, m003, m004, m005};
+
+        boolean tuttiPiuGiovani = m001.sonoTuttiPiuGiovaniDiMe(corsoMate);
+
+        System.out.println("Test metodo sonoTuttiPiuGiovaniDiMe");
+        System.out.println("Start.....");
+        System.out.println("Tutti gli studenti del corso sono più giovani di " + m001.getNome() + "? " + tuttiPiuGiovani);
+        System.out.println("End.......");
+        System.out.println(".......... \n");
     }
 }
