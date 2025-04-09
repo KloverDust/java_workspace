6d0198b9f7442d74f7c10d92271b627f75900c23
Oleksandr Chumak <ochumak@eagleprojects.it>
2025-04-09 19:00:28 +0200

    Aggiunti metodi piuEstesaDi, piuPopolosaDi, esisteAlmenoUnaPiuEstesa, dimmiQuanteSonoPiuPopolose, haPiuAbitantiDiTutteLeAltre con i rispettivi test

--- giorno3/src/it/prova/model/Nazione.java
+++ giorno3/src/it/prova/model/Nazione.java
@@ -5,6 +5,16 @@ public class Nazione {
     private double superficieKmQ;
     private int abitanti;
 
+    public Nazione () {
+
+    }
+
+    public Nazione(String denominazione, double superficieKmQ, int abitanti) {
+        this.denominazione = denominazione;
+        this.superficieKmQ = superficieKmQ;
+        this.abitanti = abitanti;
+    }
+
     @Override
     public String toString() {
         return "Nazione: " + denominazione + ", Abitanti: " + abitanti + ", Superficie: " + superficieKmQ + " km quadrati";
@@ -34,4 +44,40 @@ public class Nazione {
     public void setAbitanti(int abitanti) {
         this.abitanti = abitanti;
     }
+
+    public boolean piuEstesaDi(Nazione input) {
+        return (this.superficieKmQ > input.getSuperficieKmQ());
+    }
+
+    public boolean piuPopolosaDi(Nazione input) {
+        return (this.abitanti > input.getAbitanti());
+    }
+
+    public boolean esisteAlmenoUnaPiuEstesa(Nazione[] nazioni) {
+        for(int i=0; i< nazioni.length; i++) {
+            if(this.superficieKmQ< nazioni[i].getSuperficieKmQ())
+                return true;
+        }
+        return false;
+    }
+
+    public int dimmiQuanteSonoPiuPopolose(Nazione[] nazioni) {
+        int counter = 0;
+        for(int i=0; i<nazioni.length; i++) {
+            if(this.abitanti < nazioni[i].getAbitanti())
+                counter++;
+        }
+        return counter;
+    }
+
+    public boolean haPiuAbitantiDiTutteLeAltre(Nazione[] nazioni) {
+        int popolazioneMax = this.abitanti;
+        for (int i=0; i<nazioni.length; i++) {
+            if (popolazioneMax< nazioni[i].getAbitanti()){
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
--- giorno3/src/it/prova/test/NazioneTester.java
+++ giorno3/src/it/prova/test/NazioneTester.java
@@ -9,6 +9,52 @@ public class NazioneTester {
         Italy.setAbitanti(60000000);
         Italy.setSuperficieKmQ(302073);
 
-        System.out.println(Italy);
+        Nazione France = new Nazione();
+        France.setDenominazione("Francia");
+        France.setAbitanti(68000000);
+        France.setSuperficieKmQ(551695);
+
+        Nazione Germany = new Nazione("Germania", 357592, 83000000);
+        Nazione Poland = new Nazione("Polonia", 322575,37000000);
+        Nazione Spain = new Nazione("Spagna", 47400000, 505990);
+        Nazione Portugal = new Nazione("Portogallo", 10196709, 92226);
+
+        boolean checkPiuEstesa = Italy.piuEstesaDi(France);
+        System.out.println("Test metodo piuEstesaDi");
+        System.out.println("Start.....");
+        System.out.println(France.getDenominazione() + " è piu grande di "+ Italy.getDenominazione() + "? " + checkPiuEstesa);
+        System.out.println("End.......");
+        System.out.println(".......... \n");
+
+        boolean checkPiuPopolosa = France.piuPopolosaDi(Germany);
+        System.out.println("Test metodo piuPopolosaDi");
+        System.out.println("Start.....");
+        System.out.println(France.getDenominazione() + " ha più abitanti di " + Germany.getDenominazione() + "? " + checkPiuPopolosa);
+        System.out.println("End.......");
+        System.out.println(".......... \n");
+
+        Nazione[] elencoNazioni = {France, Germany, Poland, Spain, Portugal};
+
+        boolean checkPiuEstesaNelGruppo = Italy.esisteAlmenoUnaPiuEstesa(elencoNazioni);
+        System.out.println("Test metodo esisteAlmenoUnaPiuEstesa");
+        System.out.println("Start.....");
+        System.out.println("Esiste almeno una nazione più estesa di " + Italy.getDenominazione() + "? " + checkPiuEstesaNelGruppo);
+        System.out.println("End.......");
+        System.out.println(".......... \n");
+
+        int numeroPiuPopolose = Italy.dimmiQuanteSonoPiuPopolose(elencoNazioni);
+        System.out.println("Test metodo dimmiQuanteSonoPiuPopolose");
+        System.out.println("Start.....");
+        System.out.println("Numero di nazioni più popolose di " + Italy.getDenominazione() + ": " + numeroPiuPopolose);
+        System.out.println("End.......");
+        System.out.println(".......... \n");
+
+        boolean checkPiuPopolosaAssoluta = France.haPiuAbitantiDiTutteLeAltre(elencoNazioni);
+        System.out.println("Test metodo haPiuAbitantiDiTutteLeAltre");
+        System.out.println("Start.....");
+        System.out.println(France.getDenominazione() + " ha più abitanti di tutte le altre? " + checkPiuPopolosaAssoluta);
+        System.out.println("End.......");
+        System.out.println(".......... \n");
+
     }
 }
