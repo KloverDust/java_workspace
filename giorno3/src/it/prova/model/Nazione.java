4c7cfd9e7e7a075350fef3b914031af3c3c6e852
Oleksandr Chumak <ochumak@eagleprojects.it>
2025-04-09 16:54:54 +0200

    Aggiunta classe Nazione e NazioneTester con i metodi

--- giorno3/src/it/prova/model/Nazione.java
+++ giorno3/src/it/prova/model/Nazione.java
@@ -1,4 +1,37 @@
 package it.prova.model;
 
 public class Nazione {
+    private String denominazione;
+    private double superficieKmQ;
+    private int abitanti;
+
+    @Override
+    public String toString() {
+        return "Nazione: " + denominazione + ", Abitanti: " + abitanti + ", Superficie: " + superficieKmQ + " km quadrati";
+    }
+
+
+    public String getDenominazione() {
+        return denominazione;
+    }
+
+    public void setDenominazione(String denominazione) {
+        this.denominazione = denominazione;
+    }
+
+    public double getSuperficieKmQ() {
+        return superficieKmQ;
+    }
+
+    public void setSuperficieKmQ(double superficieKmQ) {
+        this.superficieKmQ = superficieKmQ;
+    }
+
+    public int getAbitanti() {
+        return abitanti;
+    }
+
+    public void setAbitanti(int abitanti) {
+        this.abitanti = abitanti;
+    }
 }
--- giorno3/src/it/prova/test/NazioneTester.java
+++ giorno3/src/it/prova/test/NazioneTester.java
@@ -1,4 +1,14 @@
 package it.prova.test;
 
+import it.prova.model.Nazione;
+
 public class NazioneTester {
+    public static void main (String[] args) {
+        Nazione Italy = new Nazione();
+        Italy.setDenominazione("Italia");
+        Italy.setAbitanti(60000000);
+        Italy.setSuperficieKmQ(302073);
+
+        System.out.println(Italy);
+    }
 }
