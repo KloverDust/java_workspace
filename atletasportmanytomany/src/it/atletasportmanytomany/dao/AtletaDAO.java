e199cf64f7e3fe6173dc4b86c7082205cc977701
Klover <105972935+KloverDust@users.noreply.github.com>
2025-04-27 12:08:46 +0200

    Delete atletasportmanytomany directory

--- atletasportmanytomany/src/it/atletasportmanytomany/dao/AtletaDAO.java
+++
@@ -1,6 +0,0 @@
-package it.atletasportmanytomany.dao;
-
-import it.atletasportmanytomany.model.Atleta;
-
-public interface AtletaDAO extends IBaseDAO<Atleta>{
-}
--- atletasportmanytomany/src/it/atletasportmanytomany/dao/SportDAO.java
+++
@@ -1,6 +0,0 @@
-package it.atletasportmanytomany.dao;
-
-import it.atletasportmanytomany.model.Sport;
-
-public interface SportDAO extends IBaseDAO<Sport>{
-}
